12f8ac0d13ff3072f7c2a0d29bff64953047b72c
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-06-30 07:47:08 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: 0 if it is succesful
+ */
+in main(void)
+{
+	printfu("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
