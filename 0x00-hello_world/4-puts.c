ba0e57429bf1d1ecac1730b59efbfb37286dbc1d
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-06-30 07:38:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <std.io>
+
+/**
+ *main - Prints "Programming is like builsing a multilingual puzzle"
+ *
+ * Return: 0 if its succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
