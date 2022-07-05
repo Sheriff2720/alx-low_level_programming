1b5fa29a5798785259f81b08c0eae04f16182941
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-07-05 13:52:45 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints characters
+ *
+ * Description: Prints alphabet from a to z
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_alphabet();
+
+	_putchar('\n');
+
+	return (0);
+}
