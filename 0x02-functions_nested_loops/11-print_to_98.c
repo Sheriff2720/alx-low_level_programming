c4e56ca0457ea80ab34dcb23eb766c8f4106582e
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-07-06 07:36:15 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,16 @@
 void print_to_98(int n)
 {
 	if (n < 98)
-	{
-		while (n < 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-
-		else if (n > 98)
+	else if (n > 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
-	}
 	printf("98\n");
 }
