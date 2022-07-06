3801449888e5ff6578f93cb834fcbe66ddf597f7
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-07-06 04:14:09 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints a 1 if a character is lowercase or 0 if not
+ *
+ * Description: Prints out a 1 if a charachter is lowercase otherwise 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
