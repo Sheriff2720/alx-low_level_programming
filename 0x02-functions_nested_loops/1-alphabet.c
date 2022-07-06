9498315cf267f718d7e896d9a17f2913384bb46d
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-07-06 01:28:10 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,32 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * main - prints characters
+ * print_alphabet - prints characters
  *
  * Description: Prints alphabet from a to z
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
+
+/**
+ * main - invokes print_alphabet
+ *
+ * Description: Invokes print_alphabet
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	print_alphabet();
-	_putchar('\n');
 	return (0);
 }
