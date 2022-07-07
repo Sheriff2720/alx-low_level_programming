131d1c102addc02769d0fc49cdcf4ca312728069
Muhammad Sherif <Sheriffmuha@gmail.com>
2022-07-07 08:00:01 -0700

    First commit of this repo

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase
+ *
+ * @c: character to check for the case
+ *
+ * Return: 1 if upercase, 0 if not upercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	reurn (0);
+}
